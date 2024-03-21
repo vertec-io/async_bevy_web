447cca00ca0b8cd7a004a0dcbf0b49776bcedf20
Vertec <vertec@localhost.localdomain>
2024-03-21 14:49:05 -0500

    Cleaning up some code in axum server startup

--- crates/web_server/src/lib.rs
+++ crates/web_server/src/lib.rs
@@ -36,33 +36,28 @@ impl Default for WebServer {
 
 fn start_server(runtime: ResMut<TokioTasksRuntime>, mut server: ResMut<WebServer>) {
     //READ environment variables for the host and port and update the server address
-    dotenv().ok();
-    for (key, value) in env::vars() {
-        println!("{}: {}", key, value);
-    }
-    
+    dotenv().ok();    
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string()).parse::<u32>().unwrap_or(3000);
     let server_name = env::var("SERVER_NAME").unwrap_or_else(|_| "Axum Server".to_string());
     let new_addr = format!("{}:{}", host, port).to_socket_addrs().expect("Unable to parse socket address host and port").next().expect("Could not parse socket address and port");
+    println!("Environment Variables for server:\n NAME: {}:\n HOST: {} \n PORT: {}", &server_name, &host, &port);
+    
     server.address = new_addr;
     server.port = port;
     server.server_name = server_name;
     server.socket = Some(format!("{}:{}", host, port));
-    println!("Starting {} at host {} on port {}", &server.server_name, &server.address, &server.port);
-
-    // Need to clone the server data to move it into the background task without affecting
-    // the world state
+    
+    // Need to clone the server data to move it into the background task
     let server_clone = server.clone();
     
     runtime.spawn_background_task(|mut _ctx| async move {
-        let server_clone = (move || (server_clone))();
-        println!("{} successfully started on {}",&server_clone.server_name, &server_clone.address);
-        
+        let server_clone = (move || (server_clone))();    
         let listener = tokio::net::TcpListener::bind(&server_clone.address).await.expect("Could not create TCP Listener");
         let axum_app = Router::new()
         .route("/", get(root));
 
+        println!("Starting {} at address {} on a new thread",&server_clone.server_name, &server_clone.address);
         axum::serve(listener, axum_app)
             .await
             .expect("Server shut down unexpectedly");
